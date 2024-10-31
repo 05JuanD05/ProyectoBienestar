03bbc26fd38b191adceecb326a5f773b77fc135c
Didier_Solis <134176700+solisamy@users.noreply.github.com>
2024-10-31 16:13:01 -0500

    Subiendo Code Horarios

---
+++ src/app/componentes/horarios/horario-deportivo/horario-deportivo.component.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import { Actividad } from 'src/app/modelo/Actividad';
+import { ActividadService } from 'src/app/servicios/actividad.service';
+import { Inscripcion } from 'src/app/modelo/Inscripcion';
+import { InscripcionService } from 'src/app/servicios/inscripcion.service';
+import { Usuario } from 'src/app/modelo/Usuario';
+import { SessionService } from 'src/app/servicios/session.service';
+
+@Component({
+  selector: 'app-horario-deportivo',
+  templateUrl: './horario-deportivo.component.html',
+  styleUrls: ['./horario-deportivo.component.scss']
+})
+export class HorarioDeportivoComponent {
+
+  public actividades: Actividad[] = this.acti.actividades;
+  public insc: Inscripcion | undefined;
+  public a: Actividad | undefined;
+  public usuario: Usuario = new Usuario(0, "", "", "", "", "", "", "", "", "");
+
+  constructor(
+    private acti: ActividadService,
+    private inscripcion: InscripcionService,
+    private sesSer: SessionService
+  ) { }
+
+  ngOnInit() {
+    this.listarActividades();
+    this.usuario = this.sesSer.getUser();
+  }
+
+  listarActividades() {
+    this.acti.obtenerActividades().subscribe(
+      (data) => {
+        this.actividades = data;
+      },
+      (error) => {
+        console.error('Error al obtener actividades: ', error);
+      }
+    );
+  }
+  agregar(idA: number): void {
+    // Verificar si el usuario ya está inscrito en la actividad
+    this.inscripcion.obtenerInscripcion().subscribe(
+      (inscripciones: Inscripcion[]) => {
+        // Verificar si el usuario ya está inscrito en la actividad actual
+        const yaInscrito = inscripciones.some(inscripcion =>
+          inscripcion.usuario === this.usuario.id && inscripcion.actividad_id === idA
+        );
+
+        if (yaInscrito) {
+          // Mostrar aviso de que ya está inscrito
+          alert("Ya estás inscrito en esta actividad.");
+          return; // Salir de la función si ya está inscrito
+        }
+
+        // Si no está inscrito, continuar con la inscripción
+        const confirmacion = window.confirm('¿Estás seguro de que deseas crear la inscripción?');
+        
+        if (confirmacion) {
+          this.a = this.actividades.find(objeto => objeto.id === idA);
+          if (this.a) {
+            const nuevaInscripcion = new Inscripcion(0, "Estudiante", new Date(), this.usuario.id, idA);
+            this.inscripcion.createInscripcion(nuevaInscripcion).subscribe(
+              (response) => {
+                console.log('Actividad agregada: ', response);
+              },
+              (error) => {
+                console.error('Error al agregar la actividad: ', error);
+              }
+            );
+          } else {
+            console.error('Actividad no encontrada con id: ', idA);
+          }
+        } else {
+          console.log("Inscripción cancelada.");
+        }
+      },
+      (error) => {
+        console.error('Error al obtener inscripciones: ', error);
+      }
+    );
+  }
+}
---
+++ src/app/pinicio/pinicio.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../modelo/Usuario';
+import { UsuarioService } from '../servicios/usuario.service';
+import { Router } from '@angular/router';
+import { SessionService } from '../servicios/session.service';
+
+@Component({
+  selector: 'app-pinicio',
+  templateUrl: './pinicio.component.html',
+  styleUrls: ['./pinicio.component.scss']
+})
+
+export class PInicioComponent implements OnInit { usuario: Usuario = new Usuario(0, "", "", "", "", "", "", "", "", "");
+  usuarios: Usuario[] = [];
+  passwordFieldType: string = 'password';
+  passwordIcon: string = 'fas fa-eye';
+
+  constructor(private ususer: UsuarioService, private router: Router, private sesSer: SessionService) { }
+
+  public iniciar(): void {
+    if (!this.usuario.login || !this.usuario.password) {
+      alert('Por favor, llene las credenciales antes de iniciar sesión.');
+      return;
+    }
+  this.ususer.iniciar(this.usuario.login, this.usuario.password).subscribe(
+      usua => {   
+        if (usua && usua.length > 0) {
+          this.usuario = usua[0];
+          this.sesSer.setUser(this.usuario);
+          this.navegar();
+        } else {
+          alert('Credenciales incorrectas. Intente de nuevo.');
+        }
+      },
+      error => {
+        console.error("Error en el inicio de sesión", error);
+        alert('Error al iniciar sesión. Por favor, inténtelo más tarde.');
+      }
+    ); }
+
+  public navegar() {
+    switch (this.usuario.tipo) {
+      case 'Estudiante':
+        this.irAEstudiante();
+        break;
+      case 'Instructor':
+        this.irAInstructor();
+        break;
+      case 'Coordinador':
+        this.irACoordinador();
+        break;
+      case 'Director':
+        this.irADirector();
+        break;
+      default:
+        console.log("Tipo de usuario no reconocido");
+    }
+  }
+
+  irAEstudiante() {
+    this.router.navigate(['/guiEstudiante']); }
+irACoordinador() {
+    this.router.navigate(['/guiCoordinador']); }
+
+  irAInstructor() {
+    this.router.navigate(['/guiInstructor']); }
+
+  irADirector() {
+    this.router.navigate(['/guiDirector']);
+  }
+  togglePasswordVisibility() {    if (this.passwordFieldType === 'password') {
+      this.passwordFieldType = 'text';
+      this.passwordIcon = 'fas fa-eye-slash';
+    } else {
+      this.passwordFieldType = 'password';
+      this.passwordIcon = 'fas fa-eye';
+    }
+  }
+
+  ngOnInit(): void { }
+}
